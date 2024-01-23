efa9b4fcc05b5bd329c358705273ce1f9a5d5e34
willim2023 <paul.willim.2018@gmail.com>
2024-01-23 20:33:45 -0300

    implementação da logica do material bibliografico

--- aulas/Projeto-Biblioteca/script.js
+++ aulas/Projeto-Biblioteca/script.js
@@ -4,14 +4,31 @@ class MaterialBibliografico {
         this.autor = autor;
         this.disponivel = true;
     }
+
+    realizarEmprestimo() {
+        if (this.disponivel == true) {
+            this.disponivel = false;
+            return true; //emprestimo realizado com sucesso
+        } else {
+            return false; //material ja emprestado
+        }
+    }
+
+    realizarDevolucao() {
+        if(this.disponivel == false) {
+            this.disponivel = true;
+            return true; //devolução realizada com sucesso
+        } else {
+            return false; //material já devolvido previamente
+        }
+    }
 }
 
 class Livro extends MaterialBibliografico {
     constructor(titulo, autor, genero) {
         super(titulo, autor);
         this.genero = genero;
-    }
-
+    } 
 }
 
 class Revista extends MaterialBibliografico {
